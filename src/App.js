0f3650d040adb7c78b2692ba90f29e96c7ed0d97
Govindrai024 <govindrai024@gmail.com>
2020-06-11 01:47:18 +0500

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Govind Rai Rathore.
+          Edit <code>src/App.js</code> Govind Rai Rathore saying hi to all.
         </p>
         <a
           className="App-link"
