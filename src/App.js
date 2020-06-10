08d288fc32762f2b0c576efb99d38fb5220dcac8
Govindrai024 <govindrai024@gmail.com>
2020-06-11 02:49:35 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Govind Rai Rathore saying hi to all and saying thank you for creating and love you.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello govind rai here
-        </a>
-      </header>
-    </div>
+
+    <view> 
+     Govind Rai Rathore hi
+    </view> 
   );
 }
 
